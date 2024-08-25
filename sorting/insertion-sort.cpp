bdc12368820ac0d842fef7cde532e9df90f3d07f
X9Z0 <x9z0cofd@gmail.com>
2024-08-25 23:49:09 +0530

    insertion-sort algorithm

---
+++ sorting/insertion-sort.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private:
+    void swap(vector<int> &nums, int j, int &i){
+        int temp = nums[j];
+        nums[j] = nums[i];
+        nums[i] = temp;
+    }
+public:
+    vector<int> insertionSort(vector<int>& nums) {
+        for(int i = 1; i< nums.size(); i++){
+            for(int j = i; j>0; j--){
+                if(nums[j-1] > nums[j]){
+                    swap(nums,j-1,j);
+                }
+            }
+        }
+        return nums;
+
+    }
+};
