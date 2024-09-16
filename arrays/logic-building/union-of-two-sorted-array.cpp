8f1fd07bb6e4502d9edbd9a4025a20501786d2ca
X9Z0 <x9z0cofd@gmail.com>
2024-09-16 16:10:48 +0530

    Union of two sorted arrays

---
+++ arrays/logic-building/union-of-two-sorted-array.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> unionArray(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        vector<int> unionAr;
+
+        int i = 0;
+        int j = 0;
+
+        while(i < n1 && j < n2){
+            if(nums1[i] <= nums2[j]){
+                if(unionAr.size() == 0 || unionAr.back() != nums1[i]){
+                    unionAr.push_back(nums1[i]);
+                }
+                i++;
+            }else{
+                if(unionAr.size() == 0 || unionAr.back() != nums2[j]){
+                    unionAr.push_back(nums2[j]);
+                }
+                j++;
+            }
+        }
+        while(i < n1){
+            if(unionAr.size() == 0 || unionAr.back() != nums1[i]){
+                unionAr.push_back(nums1[i]);
+            }
+            i++;
+        }
+        while(j < n2){
+            if(unionAr.size() == 0 || unionAr.back() != nums2[j]){
+                unionAr.push_back(nums2[j]);
+            }
+            j++;
+        }
+        return unionAr;
+        
+    }
+};
