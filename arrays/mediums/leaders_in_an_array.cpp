b2379ff554e6d40baf2ece3caa50317a86094c49
X9Z0 <x9z0cofd@gmail.com>
2024-09-18 04:37:13 +0530

    Leaders in an Array

---
+++ arrays/mediums/leaders_in_an_array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> leaders(vector<int>& nums) {
+        int n1 = nums.size();
+        int great = nums[n1-1];
+        vector<int> result;
+        result.push_back(great);
+        for(int i = n1 - 2; i >= 0;i--){
+            if(nums[i] > great){
+                great = nums[i];
+                result.push_back(great);
+            }
+        }
+        int i = 0;
+        int j = result.size() - 1;
+        while(i < j){
+            swap(result[i],result[j]);
+            i++;
+            j--;
+        }
+        return result; 
+    }
+};
