9e557796ff013eb421b9836e6a6f0bb741007fa2
X9Z0 <x9z0cofd@gmail.com>
2024-09-26 02:37:01 +0530

    modification

--- recursion/implementation/generateParenthesis.cpp
+++ recursion/implementation/generateParenthesis.cpp
@@ -1,21 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-private:
-  void generate(int n, vector<string> &answer, string &s) {
-    if (n == 1) {
-      s = "";
-      s.push_back('(');
-    }
-    s.push_back('(');
+void outside(int n, string &s) {
+  s.push_back('(');
+  if (n == 1) {
+    s.push_back(')');
+    return;
   }
+  s.push_back(')');
+  outside(n - 1, s);
+}
 
-public:
-  vector<string> generateParenthesis(int n) {
-    vector<string> answer;
-    string s;
-    generate(n, answer, s);
-    return answer;
+void inside(int n, string &s) {
+  s.push_back('(');
+  if (n == 1) {
+    s.push_back(')');
+    return;
   }
-};
+  outside(n - 1, s);
+  s.push_back(')');
+}
+
+void generateAnswer(int n, vector<string> &answer) {
+  string s = "";
+  if (n == 1) {
+    outside(1, s);
+    answer.push_back(s);
+    return;
+  }
+  s = "";
+  s.push_back('(');
+}
+
+vector<string> generateP(int n) {
+  vector<string> answer;
+  generateAnswer(n, answer);
+  return answer;
+}
+int main() {
+  int n = 3;
+  vector<string> answer = generateP(n);
+  for (int i = 0; i < answer.size(); i++) {
+    cout << answer[i] << endl;
+  }
+  return 0;
+}
