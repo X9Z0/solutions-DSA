7d51aa59d63e1a482ff751ba5589808cf256fa06
X9Z0 <x9z0cofd@gmail.com>
2024-10-02 05:09:25 +0530

    distance of neareast cell having one

--- graphs/distance_nearest.cpp
+++ graphs/distance_nearest.cpp
@@ -53,40 +53,87 @@ using namespace std;
 //   }
 // };
 
+// class Solution {
+// public:
+//   vector<vector<int>> nearest(vector<vector<int>> grid) {
+//     int n = grid.size();
+//     int m = grid[0].size();
+//     vector<pair<int, int>> zeros;
+//     vector<pair<int, int>> ones;
+//     vector<vector<int>> answer(n, vector<int>(m, 0));
+//     for (int i = 0; i < m; i++) {
+//       for (int j = 0; j < n; j++) {
+//         if (grid[i][j] == 1) {
+//           ones[i].first = i;
+//           ones[i].second = j;
+//         }
+//         if (grid[i][j] == 0) {
+//           zeros[i].first = i;
+//           zeros[i].second = j;
+//         }
+//       }
+//     }
+//     n = ones.size();
+//     m = zeros.size();
+//
+//     int count = 0;
+//
+//     for (int i = 0; i < n; i++) {
+//       for (int j = 0; j < m; j++) {
+//         int dis = abs(zeros[i].first - ones[j].first) +
+//                   abs(zeros[i].second - ones[j].second);
+//         count = min(count, dis);
+//       }
+//       answer[zeros[i].first][zeros[i].second] = count;
+//       count = 0;
+//     }
+//     return answer;
+//   }
+// };
+
 class Solution {
+private:
+  void bfs(vector<vector<int>> &grid, vector<vector<int>> &vis,
+           queue<pair<int, int>> &q, vector<vector<int>> &answer, int n,
+           int m) {
+    while (!q.empty()) {
+      int r = q.front().first;
+      int c = q.front().second;
+      q.pop();
+
+      int disR[] = {-1, 0, 1, 0};
+      int disC[] = {0, 1, 0, -1};
+      for (int i = 0; i < 4; i++) {
+        int nr = r + disR[i];
+        int nc = c + disC[i];
+        if (nr < n && nr >= 0 && nc < m && nc >= 0 && !vis[nr][nc] &&
+            grid[nr][nc] == 0) {
+          vis[nr][nc] = 1;
+          answer[nr][nc] = answer[r][c] + 1;
+          q.push({nr, nc});
+        }
+      }
+    }
+  }
+
 public:
   vector<vector<int>> nearest(vector<vector<int>> grid) {
     int n = grid.size();
     int m = grid[0].size();
-    vector<pair<int, int>> zeros;
-    vector<pair<int, int>> ones;
+    queue<pair<int, int>> q;
     vector<vector<int>> answer(n, vector<int>(m, 0));
-    for (int i = 0; i < m; i++) {
-      for (int j = 0; j < n; j++) {
-        if (grid[i][j] == 1) {
-          ones[i].first = i;
-          ones[i].second = j;
-        }
-        if (grid[i][j] == 0) {
-          zeros[i].first = i;
-          zeros[i].second = j;
-        }
-      }
-    }
-    n = ones.size();
-    m = zeros.size();
-
-    int count = 0;
+    vector<vector<int>> vis(n, vector<int>(m, 0));
 
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
-        int dis = abs(zeros[i].first - ones[j].first) +
-                  abs(zeros[i].second - ones[j].second);
-        count = min(count, dis);
+        if (grid[i][j] == 1) {
+          answer[i][j] = 0;
+          vis[i][j] = 1;
+          q.push({i, j});
+        }
       }
-      answer[zeros[i].first][zeros[i].second] = count;
-      count = 0;
     }
+    bfs(grid, vis, q, answer, n, m);
     return answer;
   }
 };
