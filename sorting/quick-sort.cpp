3da736ac6660890c243de56c70585e2ce51f89dd
X9Z0 <x9z0cofd@gmail.com>
2024-10-15 05:18:12 +0530

    header added

--- sorting/quick-sort.cpp
+++ sorting/quick-sort.cpp
@@ -1,34 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 private:
-    void swap(vector<int> &nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+  void swap(vector<int> &nums, int i, int j) {
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+  }
+  int partition(vector<int> &nums, int low, int high) {
+    int pivot = nums[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+      if (nums[j] <= pivot) {
+        i++;
+        swap(nums, i, j);
+      }
     }
-    int partition(vector<int> &nums, int low, int high){
-        int pivot = nums[high];
-        int i = low - 1;
-        for(int j = low; j< high; j++){
-            if(nums[j] <= pivot){
-                i++;
-                swap(nums,i,j);
-            }
-        }
-        swap(nums,i+1,high);
-        return i+1;
+    swap(nums, i + 1, high);
+    return i + 1;
+  }
+  void qsort(vector<int> &nums, int low, int high) {
+    if (low < high) {
+      int pi = partition(nums, low, high);
+      qsort(nums, low, pi - 1);
+      qsort(nums, pi + 1, high);
     }
-    void qsort(vector<int> &nums, int low, int high){
-        if(low < high){
-            int pi = partition(nums,low,high);
-            qsort(nums,low,pi-1);
-            qsort(nums,pi+1,high);
-        }
-    }
+  }
+
 public:
-    vector<int> quickSort(vector<int>& nums) {
-        int low = 0;
-        int high = nums.size() - 1;
-        qsort(nums,low,high);
-        return nums;
-    }
+  vector<int> quickSort(vector<int> &nums) {
+    int low = 0;
+    int high = nums.size() - 1;
+    qsort(nums, low, high);
+    return nums;
+  }
 };
