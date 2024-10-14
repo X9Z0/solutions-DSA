478b7ef81c654ed164755424367862e5b3b149ee
X9Z0 <x9z0cofd@gmail.com>
2024-10-15 05:17:45 +0530

    header added

--- sorting/bubble-sort.cpp
+++ sorting/bubble-sort.cpp
@@ -1,19 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 private:
-    void swap(vector<int> &nums, int i, int &j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
+  void swap(vector<int> &nums, int i, int &j) {
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+  }
+
 public:
-    vector<int> bubbleSort(vector<int>& nums) {
-        for(int i = 0; i< nums.size(); i++){
-            for(int j = 1; j< nums.size() - i; j++){
-                if(nums[j-1] > nums[j]){
-                    swap(nums,j-1,j);
-                }
-            }
+  vector<int> bubbleSort(vector<int> &nums) {
+    for (int i = 0; i < nums.size(); i++) {
+      for (int j = 1; j < nums.size() - i; j++) {
+        if (nums[j - 1] > nums[j]) {
+          swap(nums, j - 1, j);
         }
-        return nums;
+      }
     }
+    return nums;
+  }
 };
